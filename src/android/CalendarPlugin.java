854ff660d1efdff8d86f7ff7c47e016bbbe4840e
jomo0825 <jomo0825@gmail.com>
2015-08-16 00:22:35 +0800

    Update CalendarPlugin.java

--- src/android/CalendarPlugin.java
+++ src/android/CalendarPlugin.java
@@ -2,8 +2,8 @@ package com.memoryabc.cordovapluginhello;
 
 import android.content.Intent;
 
-import org.apache.cordova.api.CallbackContext;
-import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
