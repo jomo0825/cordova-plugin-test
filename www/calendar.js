434c590acb3780e347b5cdb28e25304f646b34b4
jomo0825 <jomo0825@gmail.com>
2015-08-15 23:34:18 +0800

    Create calendar.js

---
+++ www/calendar.js
@@ -0,0 +1,17 @@
+var calendarPlugin = {
+  createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
+    cordova.exec(
+      successCallback,
+      errorCallback,
+      'CalendarPlugin',
+      'addCalendarEntry',
+      [{
+        "title": title;
+        "description": notes,
+        "eventLocation": location,
+        "startTimeMills": startDate.getTime(),
+        "endTimeMills": endDate.getTime()
+      }]
+    );
+  }
+}
